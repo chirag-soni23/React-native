82076f378a5ebd8d2eb03950b270d533f9f39dcc
Chirag Soni <csoni0693@gmail.com>
2024-12-19 20:24:58 +0530

    native

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,14 +29,14 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Attendance',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="Attendance"
         options={{
-          title: 'Explore',
+          title: 'Marks',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
