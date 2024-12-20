2a43c8d0ca822408a3439d221ea70b498307d25e
Chirag Soni <csoni0693@gmail.com>
2024-12-20 19:08:14 +0530

    naive

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,14 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { FaClipboardUser } from "react-icons/fa6";
-import { FaRegListAlt } from "react-icons/fa";
 import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+import Ionicons from 'react-native-vector-icons/Ionicons'; 
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -22,7 +22,6 @@ export default function TabLayout() {
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
@@ -32,14 +31,14 @@ export default function TabLayout() {
         name="Attendance"
         options={{
           title: 'Attendance',
-          tabBarIcon: ({ color }) => <FaClipboardUser size={24} color='white'/>,
+          tabBarIcon: ({ color }) => <Ionicons name="clipboard" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
         name="index"
         options={{
           title: 'Marks',
-          tabBarIcon: ({ color }) => <FaRegListAlt size={24} color='white'/>,
+          tabBarIcon: ({ color }) => <MaterialIcons name="list-alt" size={24} color={color} />,
         }}
       />
     </Tabs>
